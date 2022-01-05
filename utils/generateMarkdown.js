93df5173dae163c1f323892b52b08c4d22dec8b8
Alexys Carrasquillo <alexys.carrasquillo@gmail.com>
2022-01-04 19:00:31 -0500

    code for README layout

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,7 +12,46 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
+
+  ![badge](https://img.shields.io/badge/license-${data.license}-brightgreen)<br />
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  -[Description](#description)
+  -[Installation](#install)
+  -[Usage](#useInfo)
+  -[License](#license)
+  -[Contributing](#contribute)
+  -[Tests](#instructions)
+  -[Issues](#issues)
+
+  ## Installation
+  ${data.install}
+
+  ## Usage
+  ${data.useInfo}
+
+  ## License
+  This application is covered by the ${data.license} license.
+
+  ## Contributing
+  ${data.contribute}
+
+  ## Tests
+  ${data.instructions}
+
+  ## Issues 
+  ${data.issues}<br />
+  <br />
+  Find me on GitHub: [${data.github}](https://github.com/${data.github})<by />
+  <br />
+  Email me about any issues or questions: 
+
+
 
 `;
 }
